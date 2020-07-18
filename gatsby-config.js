5185deb4fb5b99bd361a0103fd8c44d45387b5b3
valkyrie-max <alisakitkina2016@gmail.com>
2020-07-17 21:05:03 -0500

    switched to graphql playground

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Alisa's Odd Journey: Personal Blog`,
+    description: `This a personal project to showcase the knowledge of Gatsby as well as practice a tiny bit of React.`,
+    author: `@alisacodes`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import FooterStyling from './componentStyling/footer.module.scss'
 const Footer = () => {
     return(
         <footer>
-            <p>crafted with love, some <a className={FooterStyling.footerLink} href="https://youtu.be/2MtOpB5LlUA?t=225">quality music</a>, and <a className={FooterStyling.footerLink} href="https://www.gatsbyjs.org/">gatsby</a>. </p>
+            <p>crafted with love, some <a className={FooterStyling.footerLink} href="https://youtu.be/2MtOpB5LlUA?t=225">quality music</a>, and <a className={FooterStyling.footerLink} href="https://www.gatsbyjs.org/">gatsby</a>. <span aria-hidden="true">(°◡°♡)</span></p>
         </footer>
     )
 }
