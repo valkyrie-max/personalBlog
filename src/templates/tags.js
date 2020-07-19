61c8d7abaa9175aacea1ae686bb56102c7876271
valkyrie-max <alisakitkina2016@gmail.com>
2020-07-19 17:30:34 -0500

    finished styling the tags page

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -4,13 +4,15 @@ import {Link} from 'gatsby';
 import Layout from '../components/layout';
 import { slugify } from '../util/utilityFunctions';
 
+import TagsTemplate from './templatesStyling/tagsTemplate.module.scss'
+
 const Tags = ({pageContext}) => {
     const {tags} = pageContext;
     return (
         <Layout>
             <h2>tag list</h2>
 
-            <ul className="separateTagList">
+            <ul className={TagsTemplate.separateTagList}>
                 {tags.map(tag => (
                     <li key={tag}>
                         <Link to={`/tags/${slugify(tag)}`}>{tag}</Link>
