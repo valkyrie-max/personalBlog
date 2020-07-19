320820f16d1317c3c1fcd9e0f5a9fb3b2ba02528
valkyrie-max <alisakitkina2016@gmail.com>
2020-07-18 20:02:20 -0500

    managed to display titles, dates and tags for posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,11 +10,11 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
     },
-    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { graphql, useStaticQuery} from 'gatsby'
+
 import Layout from '../components/layout'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                            tags
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    console.log(data)
+
     return (
         <Layout>
             <h2>aoj: blog</h2>
-            <p>Posts?</p>
+            <ol>
+                {data.allMarkdownRemark.edges.map((edge) => {
+                    return (
+                        <li>
+                            <h3>{edge.node.frontmatter.title}</h3>
+                            <h4>{edge.node.frontmatter.tags}</h4>
+                            <p>{edge.node.frontmatter.date}</p>
+                        </li>
+                    )
+                })}
+            </ol>
         </Layout>
     )
 }
